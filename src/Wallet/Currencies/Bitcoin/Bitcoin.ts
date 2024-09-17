d6cd1ea7cc26d3624ce4b8e951177167b9cabcfb
Carlos Casado <cuentas@carloscasado.org>
2024-09-17 13:36:56 +0200

    Bitcoin Testnet WIP

--- src/Wallet/Currencies/Bitcoin/Bitcoin.ts
+++ src/Wallet/Currencies/Bitcoin/Bitcoin.ts
@@ -5,7 +5,7 @@ import {AddressType} from './BitcoinUtils'
 import {Address as TapScriptAddress, Tap} from '@cmdcode/tapscript'
 import {ConsumeFunction} from '../../../CGDriver'
 import {PreparedBitcoinTransfer} from './PreparedBitcoinTransfer'
-import {BitcoinApi} from 'chaingate-client'
+import {BitcoinApi, BitcoinTestnetApi} from 'chaingate-client'
 import {BitcoinFee} from './BitcoinFee'
 import * as Utils from '../../../Utils/Utils'
 
@@ -13,14 +13,16 @@ export type UTXO = {
     amount: Decimal, txid: string, n: number, script: Uint8Array
 }
 
-export class Bitcoin extends Currency {
-    declare readonly api: BitcoinApi
 
-    constructor(api: BitcoinApi, privateKeySource: PrivateKeySource) {
+abstract class BitcoinBase extends Currency {
+    declare readonly api: BitcoinApi | BitcoinTestnetApi
+    private readonly isMainnet: boolean
+
+    protected constructor(api: BitcoinApi | BitcoinTestnetApi, privateKeySource: PrivateKeySource, isMainnet: boolean) {
         super({
-            symbol: 'BTC',
-            id: 'bitcoin',
-            defaultDerivationPath: 'm/84\'/0\'/0\'/0/0',
+            symbol: isMainnet ? 'BTC': 'BTC-TESTNET',
+            id: isMainnet ? 'bitcoin' : 'bitcoin-testnet',
+            defaultDerivationPath: isMainnet ? 'm/84\'/0\'/0\'/0/0' : 'm/84\'/1\'/0\'/0/0',
             name: 'Bitcoin',
             svgLogoUrl: Utils.buildUrlWithApiKey('https://api.chaingate.dev/bitcoin/logo'),
             decimals: 8,
@@ -29,6 +31,7 @@ export class Bitcoin extends Currency {
         ,
         api,
         privateKeySource)
+        this.isMainnet = isMainnet
     }
 
     getAddress() {
@@ -38,11 +41,11 @@ export class Bitcoin extends Currency {
     async getAddressOfType(typeOfAddress: AddressType) {
         switch (typeOfAddress) {
         case AddressType.PubKeyHash:
-            return TapScriptAddress.p2pkh.fromPubKey(this.publicKey.compressed)
+            return TapScriptAddress.p2pkh.fromPubKey(this.publicKey.compressed, this.isMainnet ? 'main' : 'testnet')
         case AddressType.WitnessPubKeyHash:
-            return TapScriptAddress.p2wpkh.fromPubKey(this.publicKey.compressed)
+            return TapScriptAddress.p2wpkh.fromPubKey(this.publicKey.compressed, this.isMainnet ? 'main' : 'testnet')
         case AddressType.TapRoot:
-            return TapScriptAddress.p2tr.fromPubKey(Tap.getPubKey(this.publicKey.compressed)[0])
+            return TapScriptAddress.p2tr.fromPubKey(Tap.getPubKey(this.publicKey.compressed)[0], this.isMainnet ? 'main' : 'testnet')
         case AddressType.ScriptHash:
         case AddressType.WitnessScriptHash:
             throw new Error('Generating script addresses is currently unsupported')
@@ -54,6 +57,7 @@ export class Bitcoin extends Currency {
             this.api,
             this.api.addressBalance,
             address ?? await this.getAddress())
+        console.log(address ?? await this.getAddress())
         return {
             confirmed: new CurrencyAmount(this, new Decimal(balance.confirmed)),
             unconfirmed: new CurrencyAmount(this, new Decimal(balance.unconfirmed))
@@ -61,7 +65,7 @@ export class Bitcoin extends Currency {
     }
 
     async prepareTransfer(toAddress: string, amount: CurrencyAmount): Promise<PreparedBitcoinTransfer> {
-        if (toAddress.startsWith('bcrt1q')) throw new Error('Taproot addresses are not yet supported')
+        if (toAddress.startsWith('bc1p') || toAddress.startsWith('tb1p')) throw new Error('Taproot addresses are not yet supported')
 
         const fromAddress = await this.getAddress()
         const utxos = await this.buildUtxos(amount.baseAmount)
@@ -116,5 +120,17 @@ export class Bitcoin extends Currency {
     fee(fee: { feePerByte: CurrencyAmount } | { feeAmount: CurrencyAmount }): BitcoinFee {
         if('feePerByte' in fee) return BitcoinFee.feeAmount(fee.feePerByte)
         if('feeAmount' in fee) return BitcoinFee.feeAmount(fee.feeAmount)
+    }
+}
+
+export class Bitcoin extends BitcoinBase {
+    constructor(api: BitcoinApi, privateKeySource: PrivateKeySource) {
+        super(api, privateKeySource, true)
+    }
+}
+
+export class BitcoinTestnet extends BitcoinBase {
+    constructor(api: BitcoinApi, privateKeySource: PrivateKeySource) {
+        super(api, privateKeySource, false)
     }
 }
